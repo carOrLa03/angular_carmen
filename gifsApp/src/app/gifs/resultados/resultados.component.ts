5eba5a655f401490489d00b9323d2c85a3bfe89c
carOrLa03 <carmenorla@icloud.com>
2023-01-27 08:52:03 +0100

    interface/tipado

--- gifsApp/src/app/gifs/resultados/resultados.component.ts
+++ gifsApp/src/app/gifs/resultados/resultados.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./resultados.component.scss']
 })
 export class ResultadosComponent {
+
+
+  get resultados() {
+    return this.gifsService.resultados //creamos la propiedad en la clase mediante un get
+  }
+
+  constructor(private gifsService:GifsService) { }
 
 }
--- gifsApp/src/app/gifs/services/gifs.service.ts
+++ gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGIFResponse } from '../../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,8 @@ export class GifsService {
 
   private apiKey: string = "AC7PknJbIUyb88TbbJ0TCm5qOsf423T3";
   private _historial: string[] = []
+
+  public resultados:Gif[] = []
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,15 +22,13 @@ export class GifsService {
     if (query.trim().length === 0) return;
     if (this._historial.includes(query)) return; //si la query esta en el array nos devuelve a la pagina para volver a introducir un gif
 
-
-
     this._historial.unshift(query) //insertamos al inicio del array
     this._historial = this._historial.splice(0, 10);
     console.log(this._historial)
 
-
-    this.httpClient.get("https://api.giphy.com/v1/gifs/search?api_key=AC7PknJbIUyb88TbbJ0TCm5qOsf423T3&q=dragon ball z&limit=10")
+    this.httpClient.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=AC7PknJbIUyb88TbbJ0TCm5qOsf423T3&q=${query}&limit=10`)
       .subscribe((resp: any) => {
+        this.resultados = resp.data
         console.log(resp.data);
       })
 
