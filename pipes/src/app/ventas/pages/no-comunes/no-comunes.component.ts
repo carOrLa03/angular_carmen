203d9cea5c1dc7b737f15b2aa5c75bfc3304a874
carOrLa03 <carmenorla@icloud.com>
2023-02-21 13:16:51 +0100

    pipes no comunes

--- pipes/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ pipes/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { I18nSelectPipe } from '@angular/common';
 
 @Component({
   selector: 'app-no-comunes',
@@ -8,9 +9,28 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class NoComunesComponent {
 
+  nombre: string = "Carmen";
+  gender: string = 'female';
+  map: any = { 'male': 'invitarlo', 'female': 'invitarla', 'other': 'invitarles' };
+
+
+  clientes: string[] = ['maria', 'emilio', 'pablo', 'carmen', 'alex', 'guille', 'pau'];
+  messageMapping:
+    { [k: string]: string } = { '=0': 'no hay clientes', '=1': '1 cliente', 'other': '# clientes.' };
+
   constructor(private primengConfig: PrimeNGConfig) { }
+
   ngOnInit() {
     this.primengConfig.ripple = true;
+
+  }
+
+  cambiarpersona() {
+    this.nombre = 'Manolo';
+    this.gender = 'male';
+  }
+  borrarCliente() {
+    this.clientes.shift();
   }
 
 }
